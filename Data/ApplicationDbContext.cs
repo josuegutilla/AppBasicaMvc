a1ae5d69ffa4c111033b9c2587ad693956e472e0
Josué <gutillaj@gmail.com>
2023-03-08 00:15:37 -0400

    criando as tebelas no Banco (EF)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AppMvcBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppMvcBasica.Data
@@ -9,5 +10,9 @@ namespace AppMvcBasica.Data
             : base(options)
         {
         }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
     }
 }
--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -1,13 +1,21 @@
 ﻿using AppMvcBasica.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppMvcBasica.Models
 {
     public class Fornecedor : Entity
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
         public TipoFornecedor TipoFornecedor { get; set; } //enum;
         public Endereco Endereco { get; set; } //composição tem 1;
+
+        [Display(Name = "Ativo?")]
         public bool Ativo { get; set; }
         public IEnumerable<Produto> Produtos { get; set; } //composição tem varios;
     }
